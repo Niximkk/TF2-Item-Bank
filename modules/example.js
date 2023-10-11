e87f082ec3ee93bf27db3c3b99ef4a76f2042fd5
Im Nix <85513545+Niximkk@users.noreply.github.com>
2023-10-11 01:50:24 -0300

    Add files via upload

---
+++ modules/example.js
@@ -0,0 +1,6 @@
+const functions = require('../functions/functions');
+const defIndexJSON = require('../items/defIndex.json');
+
+module.exports = (client, tf2) => {
+    // Your custom module logic goes here.
+};
